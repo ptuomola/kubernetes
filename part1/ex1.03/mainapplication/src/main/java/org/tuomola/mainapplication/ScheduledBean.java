849f14ceac4935ed5da32738cc905845538e69f3
Petri Tuomola <petri@tuomola.org>
2020-12-30 06:59:10 +0800

    ex1.03

---
+++ part1/ex1.03/mainapplication/src/main/java/org/tuomola/mainapplication/ScheduledBean.java
@@ -0,0 +1,26 @@
+package org.tuomola.mainapplication;
+
+import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class ScheduledBean {
+	private UUID uuid;
+	private String currentStr; 
+	
+	public ScheduledBean() {
+		uuid = UUID.randomUUID();
+	}
+
+	@Scheduled(fixedDelay = 5000)
+	public void printHash() throws IOException {
+		currentStr = ZonedDateTime.now( ZoneOffset.UTC ).format( DateTimeFormatter.ISO_INSTANT )
+				+ ": "+ uuid.toString();
+		System.out.println(currentStr);
+	}
+	
+}
