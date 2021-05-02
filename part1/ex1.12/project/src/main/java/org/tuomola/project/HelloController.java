b555a812d6c4cab6e6bd5948957db4c04ae80150
Petri Tuomola <petri@tuomola.org>
2021-05-03 06:50:01 +0800

    ex1.12

---
+++ part1/ex1.12/project/src/main/java/org/tuomola/project/HelloController.java
@@ -0,0 +1,45 @@
+package org.tuomola.project;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+
+	private static String FILE_PATH = "/usr/src/app/files";
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) throws IOException {
+		var dateString = ZonedDateTime.now( ZoneOffset.UTC ).format( DateTimeFormatter.ISO_DATE );
+		var dayFileName = FILE_PATH + "/" + dateString + ".jpg";
+
+		if(!new File(dayFileName).exists()) {
+			fetchDayFile(dayFileName);
+		}
+
+		model.addAttribute("dayFileName", "images/" + dateString + ".jpg");
+
+		return "index.html";
+	}
+
+	private void fetchDayFile(String dayFileName) throws IOException
+	{
+		System.out.println("Fetching " + dayFileName);
+		try(InputStream in = new URL("https://picsum.photos/1200").openStream()) {
+			Files.copy(in, Paths.get(dayFileName));
+		}
+	}
+}
---
+++ part1/ex1.12/project/src/main/java/org/tuomola/project/Project.java
@@ -0,0 +1,23 @@
+package org.tuomola.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Project {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project.class, args);
+	}
+
+	@Configuration
+	public class StaticResourceConfiguration implements WebMvcConfigurer {
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+					registry.addResourceHandler("/images/**").addResourceLocations("file:/usr/src/app/files/");
+			}
+	}
+}
