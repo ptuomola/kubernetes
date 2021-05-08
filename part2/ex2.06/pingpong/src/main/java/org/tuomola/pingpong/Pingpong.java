49c2c5b0be5a596afbb6dc17eeed836c21de893d
Petri Tuomola <petri@tuomola.org>
2021-05-08 14:37:23 +0800

    ex2.06

---
+++ part2/ex2.06/pingpong/src/main/java/org/tuomola/pingpong/Pingpong.java
@@ -0,0 +1,12 @@
+package org.tuomola.pingpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pingpong {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pingpong.class, args);
+	}
+}
