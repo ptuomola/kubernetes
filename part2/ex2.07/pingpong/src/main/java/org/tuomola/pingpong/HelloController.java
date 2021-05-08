88d41f56dad746726c5ff6985b264aeb97876966
Petri Tuomola <petri@tuomola.org>
2021-05-09 07:14:16 +0800

    ex2.07

---
+++ part2/ex2.07/pingpong/src/main/java/org/tuomola/pingpong/HelloController.java
@@ -0,0 +1,27 @@
+package org.tuomola.pingpong;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.IOException;
+
+
+@RestController
+public class HelloController {
+        @Autowired
+        CounterService cs;
+
+	@RequestMapping("/")
+	public String index() throws IOException {
+                int counter = cs.getCounter();
+                String responseStr = "pong " + counter; 
+                counter++;
+                cs.setCounter(counter);
+                return responseStr;
+        }
+
+        @RequestMapping("/pongs")
+        public String pongs() {
+                return "Ping / Pongs: " + cs.getCounter();
+        }
+}
