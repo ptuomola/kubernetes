1f82dbfebf1114a65364679a09400fa89d93ff05
Petri Tuomola <petri@tuomola.org>
2021-05-15 06:46:10 +0800

    ex2.08

---
+++ part2/ex2.08/backend/src/main/java/org/tuomola/project/BackendController.java
@@ -0,0 +1,39 @@
+package org.tuomola.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class BackendController {
+	@Autowired
+  JdbcTemplate jdbcTemplate;
+
+	public BackendController(JdbcTemplate jdbcTemplate)
+  {
+    this.jdbcTemplate = jdbcTemplate;
+    jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS todos ( todotext VARCHAR )");
+  }
+
+
+	@GetMapping("/todos")
+	public List<String> getTodos() {
+		ArrayList<String> todos = new ArrayList<>();
+
+		jdbcTemplate.query("SELECT todotext FROM todos", 
+			(rs, rowNum) -> todos.add(rs.getString("todotext")));
+
+		return todos;
+	}
+
+	@PostMapping("/todos")
+	public void addTodo(@RequestBody String todo) {
+			jdbcTemplate.update("INSERT INTO todos VALUES (?)", todo);
+	}
+}
