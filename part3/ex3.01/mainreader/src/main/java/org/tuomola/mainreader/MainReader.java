1015e868692ede723c0d903e781249fca61f3bb7
Petri Tuomola <petri@tuomola.org>
2021-05-18 11:08:12 +0800

    ex3.01

---
+++ part3/ex3.01/mainreader/src/main/java/org/tuomola/mainreader/MainReader.java
@@ -0,0 +1,12 @@
+package org.tuomola.mainreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainReader {
+    public static void main(String[] args) throws InterruptedException {
+      SpringApplication.run(MainReader.class, args);
+    }
+    
+}
