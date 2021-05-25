b0cecd626ada7a1d5d4ef654b2c05a5f3a604c68
Petri Tuomola <petri@tuomola.org>
2021-05-25 13:19:56 +0800

    ex4.01

---
+++ part4/ex4.01/mainreader/src/main/java/org/tuomola/mainreader/StatusController.java
@@ -0,0 +1,54 @@
+package org.tuomola.mainreader;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+	@RequestMapping("/")
+	public String index() throws NoSuchAlgorithmException, URISyntaxException, IOException, InterruptedException {
+		HttpRequest request = HttpRequest.newBuilder()
+			.uri(new URI("http://pingpong-svc.exercises-namespace:2347/pongs"))
+			.GET()
+			.build();
+
+		HttpResponse<String> response = HttpClient
+			.newBuilder()
+			.build()
+			.send(request, HttpResponse.BodyHandlers.ofString());
+
+		String pingpongs = response.body();	
+		String timestamp = ZonedDateTime.now( ZoneOffset.UTC ).format( DateTimeFormatter.ISO_INSTANT );
+		String hash = getHash(pingpongs.getBytes());
+		String message = System.getenv("MESSAGE");
+
+		return message + "<p>" + timestamp + " hash: " + hash + "<p>" + pingpongs;
+	}
+	
+	private String getHash(byte[] val) throws NoSuchAlgorithmException
+	{
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] digest = md.digest(val);
+		BigInteger bigInt = new BigInteger(1,digest);
+		return bigInt.toString(16);
+	}
+
+	@RequestMapping("/healthz")
+	public String healthcheck() throws Exception {
+		return index();
+	}
+}
---
+++ part4/ex4.01/pingpong/src/main/java/org/tuomola/pingpong/HelloController.java
@@ -0,0 +1,37 @@
+package org.tuomola.pingpong;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.io.IOException;
+
+
+@RestController
+public class HelloController {
+        @Autowired
+        CounterService cs;
+
+        @RequestMapping("/") 
+        public String defaultResponse() {
+                return "hello";
+        }
+
+	@RequestMapping("/pingpong")
+	public String index() throws IOException {
+                int counter = cs.getCounter();
+                String responseStr = "pong " + counter; 
+                counter++;
+                cs.setCounter(counter);
+                return responseStr;
+        }
+
+        @RequestMapping("/pongs")
+        public String pongs() {
+                return "Ping / Pongs: " + cs.getCounter();
+        }
+
+        @RequestMapping("/healthz")
+        public String healthCheck() {
+                return pongs();
+        }
+}
